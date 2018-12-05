fca62acef4896c4d268b6c07059b38fe3306e0c0
alexandrer0 <alexandrer@yandex.ru>
2018-12-05 18:53:09 +0300

    init

--- parse.py
+++ parse.py
@@ -1,2 +1,24 @@
-from lxml import etree
-doc = etree.parse('ego_st.xml')
+from lxml import objectify
+import pandas as pd
+
+col = ['genid', 'rge', 'is_block_power_on_off']
+# df = pd.DataFrame(columns=('target_date', 'hour', 'genid', 'rge', 'is_block_power_on_off'))
+data = []
+# df = pd.DataFrame(columns=col)
+# print(df)
+
+path = 'c:\load_xml\ego_st.xml'
+xml = objectify.parse(open(path))
+root = xml.getroot()
+print(root.attrib)
+for a in root.getchildren():
+    print(a.attrib)
+    for b in a.getchildren():
+        print(b.attrib)
+        data.append(b.attrib)
+         # df = df.append(b.attrib)
+
+print(data)
+df = pd.DataFrame(data, columns=col)
+df.to_excel('xml.xlsx')
+print(df)

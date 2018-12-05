4504bd861b09e1c85c4cd6f5256b957c5a9a31c1
alexandrer0 <alexandrer@yandex.ru>
2018-12-05 20:01:31 +0300

    init

--- parse.py
+++ parse.py
@@ -1,24 +1,18 @@
 from lxml import objectify
 import pandas as pd
 
-col = ['genid', 'rge', 'is_block_power_on_off']
-# df = pd.DataFrame(columns=('target_date', 'hour', 'genid', 'rge', 'is_block_power_on_off'))
+col = ('targetdate', 'hour', 'genid', 'rge', 'is_block_power_on_off')
 data = []
-# df = pd.DataFrame(columns=col)
-# print(df)
 
 path = 'c:\load_xml\ego_st.xml'
 xml = objectify.parse(open(path))
 root = xml.getroot()
-print(root.attrib)
 for a in root.getchildren():
-    print(a.attrib)
     for b in a.getchildren():
-        print(b.attrib)
-        data.append(b.attrib)
-         # df = df.append(b.attrib)
+        q = {**a.attrib, **b.attrib}
+        data.append(q)
 
-print(data)
 df = pd.DataFrame(data, columns=col)
-df.to_excel('xml.xlsx')
+df.to_excel('c:\load_xml\pxml.xlsx', index=False)
 print(df)
+
